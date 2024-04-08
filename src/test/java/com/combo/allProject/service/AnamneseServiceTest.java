3247eebb052ccff5c81e24eefe80aa045454cbb0
Lucas França <lucasfreitas_c5@hotmail.com>
2024-04-08 13:22:30 -0300

    Testando o not found

--- src/test/java/com/combo/allProject/service/AnamneseServiceTest.java
+++ src/test/java/com/combo/allProject/service/AnamneseServiceTest.java
@@ -3,12 +3,14 @@ package com.combo.allProject.service;
 import com.combo.allProject.model.Anamnese;
 import com.combo.allProject.repository.AnamneseRepository;
 
+import org.hibernate.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.Date;
 import java.util.Optional;
@@ -43,6 +45,22 @@ class AnamneseServiceTest {
         Anamnese response = anamneseService.findById(anamnese.getId());
 
         assertEquals(Anamnese.class, response.getClass());
+    }
+
+    @Test
+    void testFindByIdNotFoundException(){
+        when(anamneseRepository.findById(anyInt())).thenThrow(new ResourceNotFoundException("Anamnese not found."));
+            //Pode ser feito desta maneira
+        try{
+            anamneseService.findById(anamnese.getId());
+        } catch (Exception e) {
+            assertEquals(ResourceNotFoundException.class, e.getClass());
+            assertEquals("Anamnese not found.", e.getMessage());
+        }
+            //OU PODE SER FEITO DESTA MANEIRA, AMBAS POSSUEM O MESMO EFEITO
+        assertThrows(ResourceNotFoundException.class, () -> {
+            anamneseService.findById(anamnese.getId());
+        }, "Anamnese not found.");
     }
 
     @Test
